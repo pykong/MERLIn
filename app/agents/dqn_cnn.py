912b7b578fd98c10bdfdb7fc8f9fc7c5216f9907
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-05-19 22:45:06 +0200

    Simplify cnn network

--- app/agents/dqn_cnn.py
+++ app/agents/dqn_cnn.py
@@ -59,9 +59,9 @@ class DQNCNNAgent(L.LightningModule):
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             nn.Flatten(),
-            nn.Linear(conv_output_size, 32),
+            nn.Linear(conv_output_size, 16),
             nn.ReLU(),
-            nn.Linear(32, self.action_space),
+            nn.Linear(16, self.action_space),
         )
 
     def remember(self, experience: Experience) -> None:

4daa86a9eb50f0c268fff153e8b496c6bf1673ff
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-05-21 10:08:08 +0200

    Add sync_vm script

---
+++ scripts/sync_vm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script to upload sources to a cloud destination for training
+# Edit sync.cfg accordingly.
+# You can also overwrite the destination.ip via command line arg.
+# ./sync_vm.sh new.destination.ip
+
+# Load configurations
+source config.cfg
+
+# Overwrite DESTINATION_IP if command line argument is provided
+if [ ! -z "$1" ]; then
+  DESTINATION_IP="$1"
+fi
+
+# Split EXCLUDE_PATTERNS into an array and prepare rsync exclude options
+IFS=' ' read -r -a EXCLUDE_PATTERNS_ARRAY <<< "$EXCLUDE_PATTERNS"
+EXCLUDE_OPTIONS=()
+for pattern in "${EXCLUDE_PATTERNS_ARRAY[@]}"; do
+  EXCLUDE_OPTIONS+=("--exclude=$pattern")
+done
+
+# Command
+rsync -avz "${EXCLUDE_OPTIONS[@]}" --include="$INCLUDE_PATTERN" "$SOURCE_DIR" "$DESTINATION_USER"@"$DESTINATION_IP":"$DESTINATION_DIR"

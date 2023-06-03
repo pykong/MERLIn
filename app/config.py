c3b5d6517fe0b786a0c153fa0f2549125ec9776c
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-06-03 23:02:55 +0200

    Change config

--- app/config.py
+++ app/config.py
@@ -6,25 +6,25 @@ class Config:
     """A configuration object holding all parameters for an experiment."""
 
     # environment parameters
-    max_episodes: int = 5_000
+    max_episodes: int = 50_000
     frame_skip: int = 4
     step_penalty: float = 0.005
 
     # agent parameters
     agent_name: str = "double_linear_dqn"
     load_agent: bool = False
-    alpha: float = 5e-3
+    alpha: float = 3e-3
     epsilon_min: float = 0.1
-    gamma: float = 1 - 5e-4  # discount factor gamma
+    gamma: float = 1 - 2e-4  # discount factor gamma
     memory_size: int = 64_000
     batch_size: int = 64
 
     # extra agent parameters
-    target_net_update_interval: int = 1000
+    target_net_update_interval: int = 1_000
 
     # save parameter
-    model_save_interval: int = 1024
-    video_record_interval: int = 1024
+    model_save_interval: int = 2048
+    video_record_interval: int = 2048
 
     # debugging
     verbose: bool = False

407a2014f0c5959b79218fde31c8004a0715f2e2
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-06-04 11:36:22 +0200

    Optimize replay buffer

--- app/utils/replay_memory.py
+++ app/utils/replay_memory.py
@@ -1,7 +1,7 @@
-import random
 from collections import deque
-from typing import NamedTuple, Self
+from typing import Deque, NamedTuple, Self
 
+import numpy as np
 from numpy import ndarray
 
 
@@ -16,14 +16,15 @@ class Transition(NamedTuple):
 class ReplayMemory:
     def __init__(self: Self, capacity: int):
         self.capacity = capacity
-        self.buffer = deque(maxlen=capacity)
+        self.buffer: Deque[Transition] = deque(maxlen=capacity)
 
     def push(self: Self, transition: Transition) -> None:
         self.buffer.append(transition)
 
     def sample(self: Self, batch_size: int) -> list[Transition]:
         sample_size = min(len(self.buffer), batch_size)
-        batch = random.sample(self.buffer, sample_size)
+        indices = np.random.choice(len(self.buffer), sample_size, replace=False)
+        batch = [self.buffer[i] for i in indices]
         return batch
 
     def __len__(self: Self) -> int:

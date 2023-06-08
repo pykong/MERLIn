b30127c147a587334a28fe08fa15c08fe0150e41
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-06-08 21:43:56 +0200

    Switch check_cuda script from tf to torch

--- scripts/check_cuda.sh
+++ scripts/check_cuda.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 nvidia-smi
-poetry run python -c "import tensorflow as tf; print(tf.__version__)"
-poetry run python -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
+nvcc --version
+poetry run python -c 'import torch; print(torch.__version__)'
+poetry run python -c 'import torch; print(f"CUDA is available: {torch.cuda.is_available()}")'
+poetry run python -c 'import torch; [print(f"{i}:{torch.cuda.get_device_name(i)}") for i in range(torch.cuda.device_count())]'

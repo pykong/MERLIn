f41da164328c843b445da847d16777d8f87880fc
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-06-10 11:45:41 +0200

    Move replay_memory to agents module

---
+++ app/agents/replay_memory.py
@@ -0,0 +1,32 @@
+from collections import deque
+from typing import Deque, NamedTuple, Self
+
+import numpy as np
+from numpy import ndarray
+
+
+class Transition(NamedTuple):
+    state: ndarray
+    action: int
+    reward: float
+    next_state: ndarray
+    done: bool
+
+
+class ReplayMemory:
+    def __init__(self: Self, capacity: int):
+        self.capacity = capacity
+        self.buffer: Deque[Transition] = deque(maxlen=capacity)
+
+    def push(self: Self, transition: Transition) -> None:
+        self.buffer.append(transition)
+
+    def sample(self: Self, batch_size: int) -> list[Transition]:
+        sample_size = min(len(self.buffer), batch_size)
+        indices = np.random.choice(len(self.buffer), sample_size - 1, replace=False)
+        batch = [self.buffer[i] for i in indices]
+        batch.append(self.buffer[-1])
+        return batch
+
+    def __len__(self: Self) -> int:
+        return len(self.buffer)

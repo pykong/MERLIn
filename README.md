78a22caceacd6440b2e0538776c4d4d4351ab093
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-06-11 13:27:39 +0200

    Extend docs

--- README.md
+++ README.md
@@ -5,10 +5,32 @@
 ### 1. Configure experiments
 
 Experiments can be defined as JSON files that are merged with the default
-configuration before being passed into the main training loop.
+configuration before being passed into the main training loop. Parameters are
+identical to the attributes of the `Config` class.
+
+Example:
+
+`experiments/experiment_one.json`
+
+```json
+{
+  "max_episodes": 1000,
+  "agent_name": "duelling_dqn",
+  "alpha": 0.05
+}
+```
+
+This will train the agent `duelling_dqn` for 1000 episodes at a learning rate
+alpha of 0.5, while all other parameters will fall back to their default values
+as defined in the `Config` class.
+
+There is a one-to-one relationship between experiment definitions and
+experiments being run. This means each JSON file in the experiment folder will
+only result in a single experiment epoch being run.
 
 ### 2. Start training
 
-Simply invoke the following command to start the training process:
+After defining at least one experiment as described in the previous section,
+start training by simply invoking the following command:
 
 `poetry run train`
--- app/config.py
+++ app/config.py
@@ -3,7 +3,49 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    """A configuration object holding all parameters for an experiment."""
+    """A configuration object holding all parameters for an experiment.
+
+    Attributes:
+    max_episodes (int): Maximum number of episodes to train on. Default is 5000.
+
+    start_epsilon_decay (int): The episode at which epsilon decay should start. Default is 1000.
+
+    env_name (str): The name of the environment to be used in the experiment. Default is 'pong'.
+
+    frame_skip (int): Number of frames to skip in the environment per action. Default is 4.
+
+    input_dim (int): The input dimension of the model. Default is 64.
+
+    num_stacked_frames (int): Number of frames to stack together as input for the model. Default is 4.
+
+    step_penalty (float): Penalty given to the agent at each step. Default is 0.0.
+
+    agent_name (str): The name of the agent to be used in the experiment. Default is 'double_dqn'.
+
+    net_name (str): The name of the neural network to be used in the experiment. Default is 'neuroips_net'.
+
+    load_agent (bool): If True, load a pre-trained agent. Default is False.
+
+    alpha (float): The learning rate of the agent. Default is 0.0001.
+
+    epsilon_min (float): The minimum epsilon value for epsilon-greedy exploration. Default is 0.1.
+
+    gamma (float): The discount factor for future rewards. Default is 0.999.
+
+    memory_size (int): The size of the replay memory. Default is 5000.
+
+    batch_size (int): The batch size for learning. Default is 32.
+
+    target_net_update_interval (int): The number of steps after which the target network should be updated. Default is 1024.
+
+    model_save_interval (int): The number of steps after which the model should be saved. Default is 2048.
+
+    video_record_interval (int): The number of steps after which a video recording should be made. Default is 2048.
+
+    verbose (bool): If True, print extra information during training. Default is False.
+
+    save_state_img (bool): If True, save images of the states during training. Default is False.
+    """
 
     # training parameters
     max_episodes: int = 5_000

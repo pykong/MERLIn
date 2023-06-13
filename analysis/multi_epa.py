f247163c3c9e7deaeeeef497e3e64bc796d2508e
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-06-13 18:43:51 +0200

    Work on multi-run epa

--- analysis/multi_epa.py
+++ analysis/multi_epa.py
@@ -1,54 +1,64 @@
 # %%
+import os
+from pathlib import Path
+from typing import Final
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-# window size for reward smoothing
-WINDOW_SIZE = 10  # Adjust this value based on your data
+# change cwd to script location
+os.chdir(Path(__file__).parent)
 
-# Initialize a list to hold all dataframes
-all_data = []
+# epsilon skip episode count
+START_EPSILON_DECAY: Final[int] = 1000
 
-# Iterate over each run for each agent
-for i in range(1, 4):
-    for j in range(1, 3):
-        # Load the data
-        agent = pd.read_csv(f"log/multi/agent_{j}_run_{i}.csv")
-        # Cut out the episodes for which epsilon was held constant
-        agent = agent.iloc[1000:]
-        # Add columns to identify the agent and run in each DataFrame
-        agent["agent"] = f"Agent {j}"
-        agent["run"] = f"Run {i}"
-        # Smooth the reward curve for this run
+# ] window size for reward smoothing
+WINDOW_SIZE: Final[int] = 10  # Adjust this value based on your data
+
+# initialize a list to hold all dataframes
+all_data: Final[list[pd.DataFrame]] = []
+
+# iterate over each run for each agent
+for run_idx in range(1, 4):
+    for agent_idx in range(1, 3):
+        # ingest data from CSV file
+        agent = pd.read_csv(f"log/multi/agent_{agent_idx}_run_{run_idx}.csv")
+        # cut out the episodes for which epsilon was held constant
+        agent = agent.iloc[START_EPSILON_DECAY:]
+        # add columns to identify the agent and run in each DataFrame
+        agent["agent"] = f"Agent {agent_idx}"
+        agent["run"] = f"Run {run_idx}"
+        # smooth the reward curve for this run
         agent["reward_smooth"] = agent["reward"].rolling(WINDOW_SIZE).mean()
         all_data.append(agent)
 
-# Combine all dataframes
+# combine all dataframes
 all_data = pd.concat(all_data, ignore_index=True)
 
-# Create a figure and a first axis for the reward
+# create a figure and a first axis for the reward
 fig, ax1 = plt.subplots()
 
-# Plot the mean reward and confidence intervals on the first y-axis
+# plot the mean reward and confidence intervals on the first y-axis
 sns.lineplot(data=all_data, x="episode", y="reward_smooth", hue="agent", ax=ax1)
 
-# Add a horizontal line at y=0
+# add a horizontal line at y=0
 ax1.axhline(0, color="grey", linestyle="--", linewidth=0.5)
 ax1.set_ylabel("Reward")
 
-# Create a second y-axis for epsilon, sharing the x-axis with the first one
+# create a second y-axis for epsilon, sharing the x-axis with the first one
 ax2 = ax1.twinx()
 
-# Plot epsilon on the second y-axis, using the epsilon values from the first agent's first run as representative
+# plot epsilon on the second y-axis, using the epsilon values of
+#  the first agent's first run as representative
 agent = all_data[(all_data["agent"] == "Agent 1") & (all_data["run"] == "Run 1")]
 sns.lineplot(data=agent, x="episode", y="epsilon", color="green", ax=ax2, legend=False)
-
 ax2.set_ylabel("Epsilon")
 
 # get the handles and labels for all lines
 handles, labels = ax1.get_legend_handles_labels()
 
-# Manually add the Epsilon label
+# manually add the Epsilon label
 labels += ["Epsilon"]
 
 # create a new legend with all lines
@@ -57,5 +67,10 @@ ax1.legend(handles=handles, labels=labels)
 plt.title("Reward and Epsilon over time")
 plt.savefig("out/smoothed_reward_epsilon_over_time_multi.svg")
 plt.show()
+
+
+def eval():
+    print("Run analysis of experimental results")
+
 
 # %%

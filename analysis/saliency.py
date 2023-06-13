fc6dacdf6c6fcdeecbeb65bc882c0ecc4de82c9a
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-06-13 19:43:43 +0200

    Work on saliency map creation

--- analysis/saliency.py
+++ analysis/saliency.py
@@ -1,30 +1,44 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from torch import nn
 
-# Assuming that you have a trained DQN model 'dqn_model' and an input state 'state'
-# Ensure that the model is in evaluation mode
-dqn_model.eval()
+from ..app.nets.ben_net import BenNet
 
-# Also assuming 'state' is a torch tensor
-state.requires_grad = True
 
-# Forward pass
-q_values = dqn_model(state)
+def plot_saliency_map(dqn_model: nn.Sequential, state) -> None:
+    # Assuming that you have a trained DQN model 'dqn_model' and an input state 'state'
+    # Ensure that the model is in evaluation mode
+    dqn_model.eval()
 
-# Take the maximum Q-value
-q_values_max = q_values.max()
+    # Also assuming 'state' is a torch tensor
+    state.requires_grad = True
 
-# Backward pass
-q_values_max.backward()
+    # Forward pass
+    q_values = dqn_model(state)
 
-# Calculate saliency map: take absolute value of gradients and then take the maximum across color channels
-saliency, _ = torch.max(state.grad.data.abs(), dim=1)
+    # Take the maximum Q-value
+    q_values_max = q_values.max()
 
-# Detach the saliency map from the computational graph and convert to numpy array
-saliency = saliency.detach().numpy()
+    # Backward pass
+    q_values_max.backward()
 
-# Plot saliency map
-plt.imshow(saliency[0], cmap=plt.cm.hot)
-plt.axis('off')
-plt.show()
+    # Calculate saliency map: take absolute value of gradients and then take the maximum across color channels
+    saliency, _ = torch.max(state.grad.data.abs(), dim=1)
+
+    # Detach the saliency map from the computational graph and convert to numpy array
+    saliency = saliency.detach().numpy()
+
+    # Plot saliency map
+    plt.imshow(saliency[0], cmap=plt.cm.hot)
+    plt.axis("off")
+    plt.savefig("out/saliency_map.svg")
+    plt.show()
+
+
+if __name__ == "__main__":
+    model = BenNet().build_net(None, None, None)
+    with open("xxx", "r") as cp_f:
+        model.load_state_dict(torch.load(cp_f))
+    state = None
+    plot_saliency_map(model, state)

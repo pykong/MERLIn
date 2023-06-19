da3d2ac9187134bf6ad54c22cece4caf1f9aae41
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-06-19 21:13:18 +0200

    Add linear net

--- app/nets/__init__.py
+++ app/nets/__init__.py
@@ -1,9 +1,10 @@
 from ._base_net import BaseNet
 from .ben_net import BenNet
 from .big_net import BigNet
+from .linear_net import LinearNet
 from .nature_net import NatureNet
 from .neuroips_net import NeuroipsNet
 
-net_registry = [BenNet, BigNet, NatureNet, NeuroipsNet]
+net_registry = [BenNet, BigNet, LinearNet, NatureNet, NeuroipsNet]
 
 __all__ = ["BaseNet", "net_registry"]
---
+++ app/nets/linear_net.py
@@ -0,0 +1,33 @@
+from torch import nn
+
+from ._base_net import BaseNet
+
+
+class LinearNet(BaseNet):
+    @classmethod
+    @property
+    def name(cls) -> str:
+        return "linear_net"
+
+    def _define_net(
+        self, state_shape: tuple[int, int, int], num_actions: int
+    ) -> nn.Sequential:
+        channel_dim, x_dim, y_dim = state_shape
+        input_dims = channel_dim * x_dim * y_dim
+        return nn.Sequential(
+            # fc 1
+            nn.Flatten(),
+            nn.Linear(input_dims, 512),
+            nn.ReLU(),
+            # fc 2
+            nn.Linear(512, 384),
+            nn.ReLU(),
+            # fc 3
+            nn.Linear(384, 64),
+            nn.ReLU(),
+            # fc 4
+            nn.Linear(64, 16),
+            nn.ReLU(),
+            # output
+            nn.Linear(16, num_actions),
+        )

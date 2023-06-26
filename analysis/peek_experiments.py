91ced5eafb3b1992d0bd1a5d3522111ca32fc564
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-06-26 21:43:57 +0200

    Improve analysis code

--- analysis/peek_experiments.py
+++ analysis/peek_experiments.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+from app.utils.file_utils import ensure_empty_dirs
+
 SMOOTH_WINDOW = 1
 
 
@@ -88,7 +90,7 @@ def plot_reward_histogram(df: pd.DataFrame, plot_file: Path) -> None:
     plt.clf()
 
 
-def save_reward_histogram(df: pd.DataFrame, output_file: Path) -> None:
+def plot_reward_histogram(df: pd.DataFrame, output_file: Path) -> None:
     # Ensure that the 'reward' and 'epsilon' columns exist
     assert "reward" in df.columns, "DataFrame must have a 'reward' column"
     assert "epsilon" in df.columns, "DataFrame must have an 'epsilon' column"
@@ -137,7 +139,7 @@ def calculate_and_write_win_rate(df: pd.DataFrame) -> str:
     # Prepare the output string
     return "\n".join(
         (
-            "-" * 7 + "Win-Rate" + "-" * 7,
+            "\n\n" + "-" * 7 + "Win-Rate" + "-" * 7,
             f"Total Episodes : {total_episodes}",
             f"Wins           : {wins}",
             f"Win Rate       : {win_rate:.2f} ({win_rate_percent:.2f}%)",
@@ -158,22 +160,30 @@ def peek(dir_: Path) -> None:
     log_files = [f for f in dir_.rglob("*.csv") if f.is_file()]
     log_files.sort()
 
+    # create sub-dirs
+    analysis_dir = dir_ / "analysis"
+    summary_dir = analysis_dir / "summaries"
+    reward_dir = analysis_dir / "reward"
+    reward_dist_dir = analysis_dir / "reward_dist"
+    ensure_empty_dirs(analysis_dir, summary_dir, reward_dir, reward_dist_dir)
+
+    # analyse individual variants
     all_frames: list[pd.DataFrame] = []
     for i, f in enumerate(log_files):
         df = pd.read_csv(f)
         exp_name = f"experiment_{i}"
         df["experiment"] = exp_name
-        save_summary(df, Path(dir_, exp_name + ".txt"))
-        save_reward_histogram(df, Path(dir_, exp_name + "_distribution" + ".svg"))
-        plot_reward(df, Path(dir_, exp_name + ".svg"))
+        save_summary(df, Path(summary_dir, exp_name + ".txt"))
+        plot_reward_histogram(df, Path(reward_dist_dir, exp_name + "_dist" + ".svg"))
+        plot_reward(df, Path(reward_dir, exp_name + ".svg"))
         all_frames.append(df)
 
     # combine all dataframes
     merged_frame = pd.concat(all_frames, ignore_index=True)
 
     # plot reward
-    plot_reward(merged_frame, dir_ / "reward_plot.svg", SMOOTH_WINDOW)
-    plot_reward_histogram(merged_frame, dir_ / "reward_hist.svg")
+    plot_reward(merged_frame, reward_dir / "all_rewards.svg", SMOOTH_WINDOW)
+    plot_reward_histogram(merged_frame, reward_dist_dir / "all_dist.svg")
 
 
 def analyze_results():

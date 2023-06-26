b3f416e2ac7cb2ad6e5cd6712c4c41a86539f6bf
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-06-26 20:49:02 +0200

    Create dqn_double_duelling agent

--- app/agents/__init__.py
+++ app/agents/__init__.py
@@ -1,8 +1,14 @@
 from ._base_agent import BaseAgent
 from .dqn_double import DoubleDQNAgent
+from .dqn_double_duelling import DoubleDuellingDQNAgent
 from .dqn_duelling import DuellingDQNAgent
 from .dqn_vanilla import VanillaDQNAgent
 
-agent_registry = [DoubleDQNAgent, DuellingDQNAgent, VanillaDQNAgent]
+agent_registry = [
+    DoubleDQNAgent,
+    DoubleDuellingDQNAgent,
+    DuellingDQNAgent,
+    VanillaDQNAgent,
+]
 
 __all__ = ["BaseAgent", "agent_registry"]

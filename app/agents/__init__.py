0133767428c38d17c1466eb8fcc5de97fb5f45d6
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-06-27 20:27:47 +0200

    Remove code related to duelling agent

--- app/agents/__init__.py
+++ app/agents/__init__.py
@@ -1,13 +1,9 @@
 from ._base_agent import BaseAgent
 from .dqn_double import DoubleDQNAgent
-from .dqn_double_duelling import DoubleDuellingDQNAgent
-from .dqn_duelling import DuellingDQNAgent
 from .dqn_vanilla import VanillaDQNAgent
 
 agent_registry = [
     DoubleDQNAgent,
-    DoubleDuellingDQNAgent,
-    DuellingDQNAgent,
     VanillaDQNAgent,
 ]
 

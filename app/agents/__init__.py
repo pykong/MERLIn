f6a98ca2755b014fa4e2c07bc1a7d15bd49828ea
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-07-01 21:46:47 +0200

    Implement double dueling dqn agent

--- app/agents/__init__.py
+++ app/agents/__init__.py
@@ -1,10 +1,12 @@
 from ._base_agent import BaseAgent
 from .dqn_double import DoubleDQNAgent
+from .dqn_double_duelling import DoubleDuelingDQNAgent
 from .dqn_dueling import DuelingDQNAgent
 from .dqn_vanilla import VanillaDQNAgent
 
 agent_registry = [
     DoubleDQNAgent,
+    DoubleDuelingDQNAgent,
     DuelingDQNAgent,
     VanillaDQNAgent,
 ]
---
+++ app/agents/dqn_double_duelling.py
@@ -0,0 +1,11 @@
+from .dqn_double import DoubleDQNAgent
+from .dqn_dueling import DuelingDQNAgent
+
+
+class DoubleDuelingDQNAgent(DuelingDQNAgent, DoubleDQNAgent):
+    """A chimeric agent combining aspects from double and dueling DQN learning."""
+
+    @classmethod
+    @property
+    def name(cls) -> str:
+        return " double_dueling_dqn"

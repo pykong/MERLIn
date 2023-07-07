c424408d319b80ffc56a3ebeb87c70f68352c227
Ben Felder <ben@felder.io>
2023-07-07 21:23:52 +0200

    Remove linear_net_bn

--- app/nets/__init__.py
+++ app/nets/__init__.py
@@ -1,13 +1,11 @@
 from ._base_net import BaseNet
 from .linear_mini_net import LinearMiniNet
 from .linear_net import LinearNet
-from .linear_net_bn import LinearNetBN
 from .nature_net import NatureNet
 
 net_registry = [
     LinearMiniNet,
     LinearNet,
-    LinearNetBN,
     NatureNet,
 ]
 

19eff4683327b784e3774d95aca9b26c1f025a58
Ben Felder <ben@felder.io>
2023-07-15 16:14:03 +0200

    Exchange memray for scalene

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-poetry run memray run -o results/memray-output.bin -m app
+OUTPUT_DIR=results/profile/
+
+poetry run scalene --reduced-profile -m app \
+&& mkdir -p $OUTPUT_DIR \
+&& mv profile.html $OUTPUT_DIR \
+&& mv profile.json $OUTPUT_DIR \
+&

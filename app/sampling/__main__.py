d04a007cb7b0f7f5520b24c6a8047e35dc795854
Ben Felder <ben@felder.io>
2023-07-25 19:46:41 +0200

    Run pre-sampling loop in multiprocess mode

--- app/sampling/__main__.py
+++ app/sampling/__main__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from typing import Final
 
 from app.sampling.config import SamplingConfig
-from app.sampling.loop import loop
+from app.sampling.loop import run_multiprocess_loop
 
 DATA_DIR: Final[Path] = Path("data")
 SAMPLE_CONFIG_PATH: Final[Path] = DATA_DIR / "sampling.json"
@@ -15,7 +15,7 @@ def load_sampling_config() -> SamplingConfig:
 def sample():
     print("Sample called.")
     config = load_sampling_config()
-    loop(config)
+    run_multiprocess_loop(config)
 
 
 if __name__ == "__main__":
--- app/sampling/loop.py
+++ app/sampling/loop.py
@@ -1,42 +1,46 @@
+import logging
+import logging.handlers
+import multiprocessing
 import pickle
 import random
+import time
 import zlib
 from pathlib import Path
+from queue import Empty
 from typing import Self
 
-from app.loop import make_env  # TODO: Relocate factory funcs
+from app.loop import make_env
 from app.memory.transition import Transition
 from app.sampling.config import SamplingConfig
 
 
 class TransitionDump:
-    def __init__(self: Self, capacity: int) -> None:
+    def __init__(self: Self, capacity: int, name: str) -> None:
         self.capacity = capacity
-        self.buffer: list[bytes] = []
+        manager = multiprocessing.Manager()
+        self.buffer = manager.list()  # shared list
+        self.name = name  # new attribute to name the file
+        self.len = multiprocessing.Value("i", 0)  # shared integer for length tracking
 
     def push(self: Self, transition: Transition) -> None:
         if not self.is_full():
             bytes_ = zlib.compress(pickle.dumps(transition))
             self.buffer.append(bytes_)
+            self.len.value += 1
 
     def __len__(self: Self) -> int:
-        return len(self.buffer)
+        return self.len.value
 
     def is_full(self: Self) -> bool:
         return len(self) >= self.capacity
 
+    def save_buffer(self: Self) -> None:  # new method to save the buffer to a file
+        path = Path(f"{self.name}.pkl")
+        with path.open("wb") as f:
+            pickle.dump(self.buffer[:], f)  # write the buffer to a file
 
-def loop(config: SamplingConfig) -> None:
-    # init dumps
-    unbiased_dump = TransitionDump(config.sample_count)
-    negative_dump = TransitionDump(config.sample_count)
-    positive_dump = TransitionDump(config.sample_count)
-    dumps = (
-        unbiased_dump,
-        negative_dump,
-        positive_dump,
-    )
 
+def loop(config: SamplingConfig, dumps: tuple) -> None:
     # create environment
     env = make_env(
         config.env_name,
@@ -61,26 +65,45 @@ def loop(config: SamplingConfig) -> None:
 
         # save experience
         transition = Transition(state, action, reward, next_state, done)
-        unbiased_dump.push(transition)
+        dumps[0].push(transition)
         if reward < 0.0:
-            negative_dump.push(transition)
+            dumps[1].push(transition)
         if reward > 0.0:
-            positive_dump.push(transition)
+            dumps[2].push(transition)
 
-        # provide output
-        print(
-            " ".join(
-                (
-                    f"S:{step:08d} :",
-                    f"U{len(unbiased_dump):06d}",
-                    f"| -{len(negative_dump):06d}",
-                    f"| +{len(positive_dump):06d}",
-                )
-            )
-        )
 
-        # update state
-        state = next_state
+def run_multiprocess_loop(config: SamplingConfig):
+    # init dumps
+    unbiased_dump = TransitionDump(config.sample_count, "unbiased_dump")
+    negative_dump = TransitionDump(config.sample_count, "negative_dump")
+    positive_dump = TransitionDump(config.sample_count, "positive_dump")
+    dumps = (
+        unbiased_dump,
+        negative_dump,
+        positive_dump,
+    )
 
-        # increment step counter
-        step += 1
+    # create processes
+    processes = []
+    for _ in range(multiprocessing.cpu_count()):
+        p = multiprocessing.Process(target=loop, args=(config, dumps))
+        processes.append(p)
+
+    # start processes
+    for p in processes:
+        p.start()
+
+    # while processes are running, print the size of the dumps
+    while any(p.is_alive() for p in processes):
+        print(f"Unbiased dump size: {len(unbiased_dump)}")
+        print(f"Negative dump size: {len(negative_dump)}")
+        print(f"Positive dump size: {len(positive_dump)}")
+        time.sleep(1)  # sleep for a while to not overload the console output
+
+    # wait for all processes to finish
+    for p in processes:
+        p.join()
+
+    # after all processes are done, save the buffers
+    for dump in dumps:
+        dump.save_buffer()

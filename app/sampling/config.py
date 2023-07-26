d5d5a17dad32a5ba5401728c83ddc09f80db9e8f
Ben Felder <ben@felder.io>
2023-07-26 08:05:35 +0200

    Set default sampling count to 100k

--- app/sampling/config.py
+++ app/sampling/config.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class SamplingConfig:
     # sampling parameters
-    sample_count: int = 100
+    sample_count: int = 100_000
 
     # environment parameters
     env_name: str = "pong"

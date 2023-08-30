a799a4261478d0ff313d35b1085744b6dc5b3f7e
Ben Felder <ben@felder.io>
2023-08-30 13:59:46 +0200

    Ensure simulation dir

--- analysis/__main__.py
+++ analysis/__main__.py
@@ -2,9 +2,9 @@ import sys
 from pathlib import Path
 
 import pandas as pd
-
 from analysis.provider.result_collector import collect_experiment_results
 from analysis.provider.result_synthesizer import synthesize_experiment_results
+from app.utils.file_utils import ensure_dirs
 
 # parameters for data synthesis
 VARIANTS = ["var_one", "var_two", "var_three", "var_four"]
@@ -20,6 +20,7 @@ def main() -> None:
     result_dir = Path(sys.argv[1])
     if "--simulate" in sys.argv:  # a poor man's CLI ;-)
         print("Simulating analysis with synthetic data.")
+        ensure_dirs(result_dir)
         result_df = synthesize_experiment_results(VARIANTS, RUN_COUNT, EPISODE_COUNT)
     else:
         result_df = collect_experiment_results(result_dir)

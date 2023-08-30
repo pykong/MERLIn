c1f8851b9c0f22c7e587b074ccda85062c005632
Ben Felder <ben@felder.io>
2023-08-30 19:22:00 +0200

    Extend summarization

--- analysis/analyzer/summarize.py
+++ analysis/analyzer/summarize.py
@@ -12,4 +12,29 @@ def summarize(result_df: pd.DataFrame, tail: int, out_file: Path) -> None:
         tail (int): Tail of episodes to use, as an assumption of plateau.
         out_file (Path): The file path to export to.
     """
-    df = result_df.tail(tail)
+    # calculate run metrics
+    run_metrics = []
+    for (variant, run), group in result_df.groupby(["variant_id", "run_id"]):
+        tail_data = group.tail(tail)
+        run_metrics.append(
+            {
+                "variant_id": variant,
+                "run_id": run,
+                "mean_reward": tail_data["reward"].mean(),
+                "max_reward": tail_data["reward"].max(),
+            }
+        )
+    run_metrics = pd.DataFrame(run_metrics)
+
+    # calculate variant metrics
+    variant_metrics = []
+    for (variant), group in run_metrics.groupby(["variant_id"]):
+        print(group.head())
+        variant_metrics.append(
+            {
+                "variant_id": variant[0],
+                "mean_reward": group["mean_reward"].mean(),
+                "max_reward": group["max_reward"].mean(),
+            }
+        )
+    pd.DataFrame(variant_metrics).to_csv(out_file)

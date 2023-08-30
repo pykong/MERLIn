58568fe7e89fc37a2cf9914c0c63ca8edb5ad786
Ben Felder <ben@felder.io>
2023-08-30 14:22:02 +0200

    Re-implement reward plotting

--- analysis/__main__.py
+++ analysis/__main__.py
@@ -2,18 +2,26 @@ import sys
 from pathlib import Path
 
 import pandas as pd
+from analysis.analyzer.plot_reward import plot_reward
 from analysis.provider.result_collector import collect_experiment_results
 from analysis.provider.result_synthesizer import synthesize_experiment_results
-from app.utils.file_utils import ensure_dirs
+from app.utils.file_utils import ensure_dirs, ensure_empty_dirs
 
-# parameters for data synthesis
+# analysis parameters
+SMOOTH_WINDOW = 25
+
+
+# parameters for result synthesis
 VARIANTS = ["var_one", "var_two", "var_three", "var_four"]
 RUN_COUNT = 3
 EPISODE_COUNT = 5_000
 
 
 def analyze(result_df: pd.DataFrame, result_dir: Path) -> None:
+    anal_dir = result_dir / "analysis"
+    ensure_empty_dirs(anal_dir)
     print(result_df.head())
+    plot_reward(result_df, anal_dir / "all_results.svg", SMOOTH_WINDOW)
 
 
 def main() -> None:
--- analysis/analyzer/peek.py
+++ analysis/analyzer/peek.py
@@ -11,47 +11,6 @@ SMOOTH_WINDOW = 25
 SKIP_FRAMES = 3500  # ignore exploration and expontential phase
 
 
-def plot_reward(df: pd.DataFrame, plot_file: Path, smooth: int | None = None) -> None:
-    df = deepcopy(df)
-    if smooth:
-        df["reward"] = df["reward"].rolling(smooth).mean()
-
-    # create a figure and a first axis for the reward
-    _, ax1 = plt.subplots()
-
-    # plot the mean reward and confidence intervals on the first y-axis
-    sns.lineplot(data=df, x="episode", y="reward", hue="experiment", ax=ax1)
-
-    # add a horizontal line at y=0
-    ax1.axhline(0, color="grey", linestyle="--", linewidth=0.5)
-    ax1.set_ylabel("Reward")
-
-    # create a second y-axis fr epsilon, sharing the x-axis with the first one
-    ax2 = ax1.twinx()
-
-    # plot epsilon on the second y-axis, using the epsilon values of
-    #  the first agent's first run as representative
-    sns.lineplot(
-        data=df[(df["experiment"] == "experiment_0")],
-        x="episode",
-        y="epsilon",
-        color="green",
-        ax=ax2,
-        legend=False,  # type:ignore
-    )
-    ax2.set_ylabel("Epsilon")
-
-    # get the handles and labels for all lines
-    handles, labels = ax1.get_legend_handles_labels()
-
-    # manually add the Epsilon label
-    labels += ["Epsilon"]
-
-    # create a new legend with all lines
-    ax1.legend(handles=handles, labels=labels)
-
-    plt.title(f"{'Smoothed' if smooth else ''} Reward and Epsilon over time")
-    plt.savefig(plot_file)
 
 
 def plot_loss(df: pd.DataFrame, plot_file: Path, smooth: int | None = None) -> None:

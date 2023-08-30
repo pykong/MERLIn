911fab154f67b45e34cc1267aabe287b9f9c8e7a
Ben Felder <ben@felder.io>
2023-08-30 20:10:33 +0200

    Improve histogram plotting

--- analysis/analyzer/plot_reward_dist.py
+++ analysis/analyzer/plot_reward_dist.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 
-def plot_reward_distribution(data: pd.DataFrame, out_file: Path) -> None:
+def plot_reward_distribution(data: pd.DataFrame, tail: int, out_file: Path) -> None:
     """
     Plot the reward distribution of each experiment as stacked histograms.
 
@@ -13,20 +14,24 @@ def plot_reward_distribution(data: pd.DataFrame, out_file: Path) -> None:
     """
     variants = data["variant_id"].unique()
 
+    tail_df = data.groupby(["variant_id", "run_id"]).tail(tail)
+
+    colors = plt.cm.viridis_r(np.linspace(0, 1, len(variants)))
+
     figsize = (10, len(variants) * 5)
     _, axes = plt.subplots(len(variants), 1, sharex=True, figsize=figsize)
 
-    for ax, variant in zip(axes, variants):
-        subset = data[data["variant_id"] == variant]
+    for ax, experiment, color in zip(axes, variants, colors):
+        subset = tail_df[tail_df["variant_id"] == experiment]
         ax.hist(
             subset["reward"],
             bins=43,
             range=(-21, 22),
             alpha=0.7,
-            color="skyblue",
+            color=color,
             edgecolor="black",
         )
-        ax.set_title(f"Reward Distribution for {variant}")
+        ax.set_title(f"Reward Distribution for {experiment}")
         ax.set_ylabel("Frequency")
 
     plt.xlabel("Reward")

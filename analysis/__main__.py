41db503622398322dc4dd58067a31bbe75d8d224
Ben Felder <ben@felder.io>
2023-08-30 13:55:48 +0200

    Introduce simulate analysis cli flag

--- analysis/__main__.py
+++ analysis/__main__.py
@@ -1,6 +1,8 @@
 import sys
 from pathlib import Path
 
+import pandas as pd
+
 from analysis.provider.result_collector import collect_experiment_results
 from analysis.provider.result_synthesizer import synthesize_experiment_results
 
@@ -10,13 +12,18 @@ RUN_COUNT = 3
 EPISODE_COUNT = 5_000
 
 
-def main():
-    result_dir = Path(sys.argv[1])  # point to result dir
-    result_df = collect_experiment_results(result_dir)
+def analyze(result_df: pd.DataFrame, result_dir: Path) -> None:
     print(result_df.head())
 
-    syn_result_df = synthesize_experiment_results(VARIANTS, RUN_COUNT, EPISODE_COUNT)
-    print(syn_result_df.head())
+
+def main() -> None:
+    result_dir = Path(sys.argv[1])
+    if "--simulate" in sys.argv:  # a poor man's CLI ;-)
+        print("Simulating analysis with synthetic data.")
+        result_df = synthesize_experiment_results(VARIANTS, RUN_COUNT, EPISODE_COUNT)
+    else:
+        result_df = collect_experiment_results(result_dir)
+    analyze(result_df, result_dir)
 
 
 if __name__ == "__main__":

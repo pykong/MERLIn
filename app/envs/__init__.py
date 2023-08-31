89119aed04fb08155c90389b34bbf4c7e926f38d
Ben Felder <ben@felder.io>
2023-08-31 20:02:49 +0200

    Move factory methods

--- app/envs/__init__.py
+++ app/envs/__init__.py
@@ -1,6 +1,22 @@
+from typing import Any
+
 from app.envs._base_env import BaseEnvWrapper
 from app.envs.pong_env import PongEnvWrapper
 
 env_registry = [PongEnvWrapper]
 
-__all__ = ["BaseEnvWrapper", "env_registry"]
+
+def make_env(name: str, **kwargs: Any) -> BaseEnvWrapper:
+    """Create environment wrapper of provided name.
+
+    Args:
+        name (str): The identifier string of the environment wrapper.
+
+    Returns:
+        BaseEnvWrapper: A wrapper instance of the environment.
+    """
+    env_ = [e for e in env_registry if e.name == name][0]
+    return env_(**kwargs)
+
+
+__all__ = ["BaseEnvWrapper", "make_env"]
--- app/loop.py
+++ app/loop.py
@@ -1,17 +1,17 @@
 import random
 from pathlib import Path
-from typing import Any, Final
+from typing import Final
 
 import cv2 as cv
 import numpy as np
 import torch
 from gym.wrappers.monitoring import video_recorder as vr
 
-from app.agents import DqnBaseAgent, agent_registry
+from app.agents import DqnBaseAgent, make_agent
 from app.config import Config
-from app.envs import BaseEnvWrapper, env_registry
+from app.envs import BaseEnvWrapper, make_env
 from app.memory import Transition
-from app.nets import BaseNet, net_registry
+from app.nets import BaseNet, make_net
 from app.utils.file_utils import ensure_empty_dirs
 from app.utils.logging import EpisodeLog, EpisodeLogger, LogLevel
 from app.utils.silence_stdout import silence_stdout
@@ -25,49 +25,8 @@ def take_picture_of_state(state: np.ndarray, f_name: Path) -> None:
         f_name (Path): The file path to save to.
     """
     state_transposed = np.transpose(state, (1, 2, 0))
-    state_transposed *= 255  # increase brightness
+    state_transposed *= 255  # type:ignore | increase brightness
     cv.imwrite(str(f_name), state_transposed)
-
-
-def make_env(name: str, **kwargs: Any) -> BaseEnvWrapper:
-    """Create environment wrapper of provided name.
-
-    Args:
-        name (str): The identifier string of the environment wrapper.
-
-    Returns:
-        BaseEnvWrapper: A wrapper instance of the environment.
-    """
-    env_ = [e for e in env_registry if e.name == name][0]
-    return env_(**kwargs)
-
-
-def make_net(name: str) -> BaseNet:
-    """Create neural net of provided name.
-
-    Args:
-        name (str): The identifier string of the neural network.
-
-    Returns:
-        BaseNet: The neural network instance.
-    """
-    net = [net for net in net_registry if net.name == name][0]
-    return net()
-
-
-def make_agent(agent_name: str, net_name: str, **kwargs: Any) -> DqnBaseAgent:
-    """Create agent of provided name and inject neural network.
-
-    Args:
-        agent_name (str): The identifier string of the agent.
-        net_name (str): The identifier string of the neural network.
-
-    Returns:
-        DqnBaseAgent: The agent instance.
-    """
-    agent_ = [a for a in agent_registry if a.name == agent_name][0]
-    kwargs["net"] = make_net(net_name)  # HACK: This is dirty :-|
-    return agent_(**kwargs)
 
 
 def run_episode(
@@ -160,7 +119,7 @@ def loop(config: Config, result_dir: Path) -> None:
     # create the policy network
     agent: DqnBaseAgent = make_agent(
         config.agent_name,
-        config.net_name,
+        net=make_net(config.net_name),
         state_shape=input_shape,
         action_space=env.action_space.n,  # type: ignore
         gamma=config.gamma,
@@ -211,8 +170,7 @@ def loop(config: Config, result_dir: Path) -> None:
             (config.model_save_interval and episode % config.model_save_interval == 0)
             or episode == config.episodes  # always save at end of epoch
         ):
-            model_name = f"{env.name}__{agent.name}__{config.net_name}__{episode}.pth"
-            model_file = model_dir / model_name
+            model_file = model_dir / f"{episode}.pth"
             logger.log(f"Saving model: {model_file}", LogLevel.SAVE)
             agent.save(model_file)
 

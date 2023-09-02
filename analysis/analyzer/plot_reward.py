6d968604bf759ff4dbffccc8e10f76c26bec3349
Ben Felder <ben@felder.io>
2023-09-03 00:55:25 +0200

    Consistently color epsilon in plots

--- analysis/analyzer/plot_reward.py
+++ analysis/analyzer/plot_reward.py
@@ -1,13 +1,14 @@
-import sys
 from copy import deepcopy
 from pathlib import Path
+from typing import Final
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from analysis.analyzer.utils.coloring import generate_color_mapping
 from matplotlib.lines import Line2D
 
-from analysis.analyzer.utils.coloring import generate_color_mapping
+EPSILON_COLOR: Final[str] = "#D95F02"
 
 
 def plot_reward(df: pd.DataFrame, plot_file: Path, smooth: int | None = None) -> None:
@@ -24,12 +25,18 @@ def plot_reward(df: pd.DataFrame, plot_file: Path, smooth: int | None = None) ->
 
     # plot the mean reward and confidence intervals on the first y-axis
     sns.lineplot(
-        data=df, x="episode", y="reward", hue="variant", ax=ax1, palette=color_map
+        data=df,
+        x="episode",
+        y="reward",
+        hue="variant",
+        ax=ax1,
+        palette=color_map,
+        linewidth=2,
     )
 
     # add a horizontal line at y=0
     ax1.axhline(0, color="grey", linestyle="--", linewidth=0.5)
-    ax1.set_ylabel("Reward")
+    ax1.set_ylabel("reward")
 
     # create a second y-axis for epsilon, sharing the x-axis with the first one
     ax2 = ax1.twinx()
@@ -40,9 +47,10 @@ def plot_reward(df: pd.DataFrame, plot_file: Path, smooth: int | None = None) ->
         data=epsilons,
         x="episode",
         y="epsilon",
-        color="pink",
+        color=EPSILON_COLOR,
         ax=ax2,
         legend=False,  # type:ignore
+        linewidth=2,
     )
     ax2.set_ylabel("epsilon")
 
@@ -50,14 +58,14 @@ def plot_reward(df: pd.DataFrame, plot_file: Path, smooth: int | None = None) ->
     handles_ax1, labels_ax1 = ax1.get_legend_handles_labels()
 
     # manually add the Epsilon label (workaround)
-    epsilon_line = Line2D([0], [0], color="purple", lw=1)
+    epsilon_line = Line2D([0], [0], color=EPSILON_COLOR, lw=1)
     handles_ax1.append(epsilon_line)
-    labels_ax1.append("Epsilon")
+    labels_ax1.append("epsilon")
 
     # create a new legend with all lines
     ax1.legend(handles=handles_ax1, labels=labels_ax1)
 
     # create plot
+    # plt.figure(figsize=(9, 5))
     plt.title(f"{'Smoothed' if smooth else ''} Reward and Epsilon over time")
-    plt.savefig(plot_file)
     plt.savefig(plot_file)

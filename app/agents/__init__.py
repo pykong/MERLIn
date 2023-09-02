08c0eddd707cfac6ac3fd7dc7264291226a1ca2f
Ben Felder <ben@felder.io>
2023-09-02 19:29:44 +0200

    Remove double dueling q network

--- app/agents/__init__.py
+++ app/agents/__init__.py
@@ -2,14 +2,12 @@ from typing import Any
 
 from app.agents._dqn_base_agent import DqnBaseAgent
 from app.agents.dqn_double import DoubleDQNAgent
-from app.agents.dqn_double_duelling import DoubleDuelingDQNAgent
 from app.agents.dqn_dueling import DuelingDQNAgent
 from app.agents.dqn_vanilla import VanillaDQNAgent
 from app.agents.random_walker import RandomWalkerAgent
 
 agent_registry = [
     DoubleDQNAgent,
-    DoubleDuelingDQNAgent,
     DuelingDQNAgent,
     VanillaDQNAgent,
     RandomWalkerAgent,

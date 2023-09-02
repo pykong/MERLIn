8ec42eb5fc9a027b765053e98db9e6da711bd46f
Ben Felder <ben@felder.io>
2023-09-03 00:16:31 +0200

    Change from histogram to violin plots

--- analysis/analyzer/plot_reward_dist.py
+++ analysis/analyzer/plot_reward_dist.py
@@ -1,54 +1,44 @@
 from pathlib import Path
 
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
+import seaborn as sns
 
 from analysis.analyzer.utils.coloring import generate_color_mapping
 
 
 def plot_reward_distribution(data: pd.DataFrame, tail: int, out_file: Path) -> None:
     """
-    Plot the reward distributions of the experiments as stacked histograms.
+    Plot the reward distribution of each experiment as violin plots.
 
     Args:
         data (pd.DataFrame): The frame holding the experimental data.
+        tail (int): The number of episodes from the end to consider.
+        out_file (Path): The file path to save the figure.
     """
-    variants = data["variant"].unique()
-
+    # get the last X episodes
     tail_df = data.groupby(["variant", "run"]).tail(tail)
+
+    # define colors for each variant
+    variants = tail_df["variant"].unique()
 
     # create color map
     color_map = generate_color_mapping(variants)  # type:ignore
 
-    # Calculate number of rows for the 2-column layout
-    nrows = len(variants) // 2
-    figsize = (15, nrows * 6)
-    fig, axes = plt.subplots(nrows, 2, sharex=True, figsize=figsize)
+    # set up the figure and axes
+    plt.figure(figsize=(12, 8))
+    sns.violinplot(
+        x="variant", y="reward", data=tail_df, palette=color_map, inner="quartile"
+    )
 
-    # Flatten the axes array for easier iteration
-    flat_axes = axes.ravel()
-
-    for ax, variant in zip(flat_axes, variants):
-        subset = tail_df[tail_df["variant"] == variant]
-        ax.hist(
-            subset["reward"],
-            bins=43,
-            range=(-21, 22),
-            alpha=0.7,
-            color=color_map[variant],
-            edgecolor="black",
-        )
-        ax.set_title(variant)
-        ax.set_xlabel("Reward")
-        ax.set_ylabel("Frequency")
-
-    # if the number of experiments is odd, hide the last unused subplot
-    if len(variants) % 2 == 1:
-        flat_axes[-1].axis("off")
-
-    # Set the main title for the entire figure
-    fig.suptitle("Reward distributions for DQN Architectures", fontsize=24, y=0.99)
-
+    # set title and labels
+    plt.title("Reward distributions for DQN Architecture")
+    plt.xlabel("Experiment")
+    plt.ylabel("Reward")
+    plt.xticks(rotation=45)
+    plt.ylim(-21, 21)
+    plt.axhline(0, color="grey", linestyle="--", linewidth=0.5)
     plt.tight_layout()
+
+    # save the figure
     plt.savefig(out_file)
